a62c6d9e7e27667e4d754d41197ef601db4dda01
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-05-06 21:23:34 -0400

    updated minio service to update in database

--- app/services/minio_service.py
+++ app/services/minio_service.py
@@ -4,6 +4,8 @@ import uuid
 from minio import Minio
 from settings.config import settings
 from fastapi import UploadFile
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user_model import User
 
 
 class MinioService:
@@ -12,22 +14,26 @@ class MinioService:
             settings.minio_host,
             access_key=settings.minio_access_key,
             secret_key=settings.minio_secret_key,
+            secure=False
         )
         self.bucket_name = settings.minio_bucket_name
 
-    async def upload_profile_picture(self, file: UploadFile) -> str:
+    async def upload_profile_picture(self, user:User,  file: UploadFile, session: AsyncSession) -> str:
         extension = file.filename.split('.')[-1]
         fileName = f"{uuid.uuid4()}.{extension}"
         content = await file.read()
 
-        if not self.client.bucket_exists(self.bucket_name):
-            self.client.make_bucket(self.bucket_name)
+        if not self.minio_client.bucket_exists(self.bucket_name):
+            self.minio_client.make_bucket(self.bucket_name)
 
-        self.client.put_object(
+        self.minio_client.put_object(
             self.bucket_name,
             fileName,
             io.BytesIO(content),
             length=len(content),
             content_type=file.content_type
         )
-        return f"{settings.minio_url}/{self.bucket_name}/{fileName}"
+        profile_picture_url =f"{settings.minio_url}/{self.bucket_name}/{fileName}"
+        user.profile_picture_url = profile_picture_url
+        await session.commit()
+        return profile_picture_url
